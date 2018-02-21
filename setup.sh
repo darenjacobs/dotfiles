e89ae880a6d0cef23d20c12c8712cbb73518b180
Daren Jacobs <ec2-user@ip-172-31-26-56.us-east-2.compute.internal>
2018-02-21 16:29:00 +0000

    add setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mv $HOME/dotfiles $HOME/.dotfiles
+ln -s $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
+ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/.dotfiles/.vim $HOME/.vim
+ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
