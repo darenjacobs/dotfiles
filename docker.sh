3f3d61f14ea9556f767ee5cb092b9d875166e17a
darenjacobs <darenjacobs@msn.com>
2019-09-24 13:42:27 +0000

    docker update

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get purge docker lxc-docker docker-engine docker.io
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable"
 sudo apt-get update
