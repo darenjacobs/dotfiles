baa42ed07535767d80c634b8495877fbce56fd6b
Daren Jacobs <darenjacobs@msn.com>
2024-03-14 04:28:56 -0400

    update update.sh

--- setup.sh
+++ setup.sh
@@ -32,11 +32,10 @@ os_type=$(uname -a |cut -f1 -d' ')
 
 if [ "${os_type}" == "Darwin" ]; then
     ln -s $HOME/.dotfiles/.bash_profile.mac $HOME/.bash_profile
-    ln -s $HOME/.dotfiles/.bashrc.mac $HOME/.bashrc
 else
     ln -s $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
-    ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 fi
+ln -s $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -s $HOME/.dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/.dotfiles/.vim $HOME/.vim
 ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
